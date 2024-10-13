f1c226954b45c3e04e3905b54137d3ba760f0140
Hanntuu <raihanrrdarmawan@gmail.com>
2024-10-13 23:20:00 +0700

    feat : add navbardashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,18 +1,20 @@
 import type { Metadata } from "next";
-import "../globals.css";
 import { poppins } from "@/styles/font";
+import "../globals.css";
+import Header from "@/components/Navbar";
 
 export const metadata: Metadata = {
-    title: "Dashboard",
-}
-export default function RootLayout({
+  title: "Dashboard",
+};
+export default async function DashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className}`}>{children}</body>
+      <Header />
+      <body className={`${poppins.className} py-8`}>{children}</body>
     </html>
   );
 }
