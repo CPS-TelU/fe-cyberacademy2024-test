1f45c99e3c9f5bb975fb35e1d162d8aaf1d01f53
Athalah Rafif Irsyach Sarbrani <sarbrani.athallah@gmail.com>
2024-10-16 17:17:49 +0700

    fix : login

---
+++ app/login/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { poppins } from "@/styles/font";
+import "../globals.css";
+
+
+export const metadata: Metadata = {
+  title: "Dashboard",
+};
+export default async function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      
+      <body className={`${poppins.className} py-8`}>
+        {children}
+        </body>
+        
+    </html>
+  );
+}
---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import LoginPage from "@/components/contents/Login";
+
+
+
+export default function Login() {
+    return (
+        <main>
+            <LoginPage/>
+        </main>
+    )
+
+}
