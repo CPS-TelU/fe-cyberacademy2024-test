04f7758e3c28637963e11813c5d3d3e611b2b4a1
Athalah Rafif Irsyach Sarbrani <sarbrani.athallah@gmail.com>
2024-10-18 18:11:32 +0700

    del : Password

--- components/contents/Registration.tsx
+++ components/contents/Registration.tsx
@@ -264,17 +264,6 @@ const RegistrationPage = () => {
           />
         </div>
         <div className="mb-4">
-          <label className="block text-sm font-medium">Password</label>
-          <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-            placeholder="Password"
-          />
-        </div>
-        <div className="mb-4">
           <label className="block text-sm font-medium">Document</label>
           <input
             type="text"
