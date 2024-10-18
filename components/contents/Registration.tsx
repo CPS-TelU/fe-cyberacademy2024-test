cf1b502d2a4589ef0a37507a6db0bd7dd223fbc4
Hanntuu <raihanrrdarmawan@gmail.com>
2024-10-18 15:23:45 +0700

    feat : integrate Registration API

--- components/contents/Registration.tsx
+++ components/contents/Registration.tsx
@@ -2,21 +2,30 @@
 
 import { poppins } from "@/styles/font";
 import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 
 const RegistrationPage = () => {
-  const [selectedFaculty, setSelectedFaculty] = useState("Select you faculty");
-  const [selectedYear, setSelectedYear] = useState("Select an option");
-  const [selectedGender, setSelectedGender] = useState("Select an option");
+  const [formData, setFormData] = useState({
+    name: "",
+    nim: "",
+    className: "",
+    email: "",
+    noHp: "",
+    gender: "Select an option",
+    faculty: "Select your faculty",
+    year: "Select an option",
+    major: "",
+    password: "",
+    document: "",
+    github: "",
+  });
+
   const [showFacultyDropdown, setShowFacultyDropdown] = useState(false);
   const [showYearDropdown, setShowYearDropdown] = useState(false);
   const [showGenderDropdown, setShowGenderDropdown] = useState(false);
 
-  const faculties = [
-    "Faculty of Electrical Engineering",
-    "Faculty of Industrial Engineering",
-  ];
-
-  const years = ["2022", "2023", "2024"];
+  const faculties = ["Electrical Engineering", "Industrial Engineering"];
+  const years = ["2021", "2022", "2023", "2024"];
   const genders = ["Male", "Female"];
 
   const facultyRef = useRef<HTMLDivElement>(null);
@@ -43,6 +52,33 @@ const RegistrationPage = () => {
     };
   }, []);
 
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleDropdownSelect = (field: string, value: string) => {
+    setFormData({ ...formData, [field]: value });
+  };
+
+  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const REGISTRATION_API_URL = `${API_BASE_URL}/auth/register`;
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("Form Data:", formData);
+    try {
+      const response = await axios.post(
+        // "https://be-cyber-academy.vercel.app/api/auth/register",
+        REGISTRATION_API_URL,
+        formData
+      );
+      console.log(response.data);
+      alert("Registration successful!");
+    } catch (error) {
+      console.error("Error registering:", error);
+      alert("Failed to register. Please try again.");
+    }
+  };
+
   return (
     <div className={`min-h-screen flex flex-col items-center justify-center p-4 ${poppins.className}`}>
       <div className="text-center mb-5">
@@ -53,13 +89,16 @@ const RegistrationPage = () => {
           The first step to start your journey
         </p>
       </div>
-      <form className="bg-white px-6 md:px-16 py-8 rounded-2xl shadow-2xl w-full max-w-lg md:max-w-4xl mb-8 border border-gray-100">
+      <form onSubmit={handleSubmit} className="bg-white px-6 md:px-16 py-8 rounded-2xl shadow-2xl w-full max-w-lg md:max-w-4xl mb-8 border border-gray-100">
         <div className="mb-4">
           <label className="block text-sm font-medium">Name</label>
           <input
             type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
             className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-            placeholder="Jajang Pargoy"
+            placeholder="Sayyid"
           />
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
@@ -67,16 +106,22 @@ const RegistrationPage = () => {
             <label className="block text-sm font-medium">NIM</label>
             <input
               type="text"
+              name="nim"
+              value={formData.nim}
+              onChange={handleChange}
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-              placeholder="1010XXXX"
+              placeholder="1101213340"
             />
           </div>
           <div>
             <label className="block text-sm font-medium">Class</label>
             <input
               type="text"
+              name="className"
+              value={formData.className}
+              onChange={handleChange}
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-              placeholder="TK-xx-xx"
+              placeholder="TT-45-09"
             />
           </div>
         </div>
@@ -84,18 +129,23 @@ const RegistrationPage = () => {
           <label className="block text-sm font-medium">E-Mail</label>
           <input
             type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
             className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-            placeholder="jeremy.jajang@gmail.com"
+            placeholder="sayyid@gmail.com"
           />
         </div>
-
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
           <div>
             <label className="block text-sm font-medium">WhatsApp Number</label>
             <input
               type="text"
+              name="noHp"
+              value={formData.noHp}
+              onChange={handleChange}
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-              placeholder="08xxxxx"
+              placeholder="085161735214"
             />
           </div>
           <div ref={genderRef} className="relative mb-4">
@@ -104,7 +154,7 @@ const RegistrationPage = () => {
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg cursor-pointer"
               onClick={() => setShowGenderDropdown(!showGenderDropdown)}
             >
-              {selectedGender}
+              {formData.gender}
             </div>
             {showGenderDropdown && (
               <ul className="absolute z-10 w-full bg-white border border-gray-300 rounded-lg mt-1 max-h-60 overflow-auto">
@@ -113,7 +163,7 @@ const RegistrationPage = () => {
                     key={index}
                     className="px-4 py-2 hover:bg-red-100 cursor-pointer"
                     onClick={() => {
-                      setSelectedGender(gender);
+                      handleDropdownSelect("gender", gender);
                       setShowGenderDropdown(false);
                     }}
                   >
@@ -124,7 +174,6 @@ const RegistrationPage = () => {
             )}
           </div>
         </div>
-
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4">
           <div ref={facultyRef} className="relative mb-4">
             <label className="block text-sm font-medium">Faculty</label>
@@ -132,7 +181,7 @@ const RegistrationPage = () => {
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg cursor-pointer"
               onClick={() => setShowFacultyDropdown(!showFacultyDropdown)}
             >
-              {selectedFaculty}
+              {formData.faculty}
             </div>
             {showFacultyDropdown && (
               <ul className="absolute z-10 w-full bg-white border border-gray-300 rounded-lg mt-1 max-h-60 overflow-auto">
@@ -141,7 +190,7 @@ const RegistrationPage = () => {
                     key={index}
                     className="px-4 py-2 hover:bg-red-100 cursor-pointer"
                     onClick={() => {
-                      setSelectedFaculty(faculty);
+                      handleDropdownSelect("faculty", faculty);
                       setShowFacultyDropdown(false);
                     }}
                   >
@@ -151,14 +200,13 @@ const RegistrationPage = () => {
               </ul>
             )}
           </div>
-
           <div ref={yearRef} className="relative mb-4">
             <label className="block text-sm font-medium">Year of Enrollment</label>
             <div
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg cursor-pointer"
               onClick={() => setShowYearDropdown(!showYearDropdown)}
             >
-              {selectedYear}
+              {formData.year}
             </div>
             {showYearDropdown && (
               <ul className="absolute z-10 w-full bg-white border border-gray-300 rounded-lg mt-1 max-h-60 overflow-auto">
@@ -167,7 +215,7 @@ const RegistrationPage = () => {
                     key={index}
                     className="px-4 py-2 hover:bg-red-100 cursor-pointer"
                     onClick={() => {
-                      setSelectedYear(year);
+                      handleDropdownSelect("year", year);
                       setShowYearDropdown(false);
                     }}
                   >
@@ -178,33 +226,53 @@ const RegistrationPage = () => {
             )}
           </div>
         </div>
-
         <div className="mb-4">
           <label className="block text-sm font-medium">Major</label>
           <input
             type="text"
+            name="major"
+            value={formData.major}
+            onChange={handleChange}
             className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-            placeholder="S1 - Teknik xxxxx"
+            placeholder="Telecommunication Engineering"
+          />
+        </div>
+        <div className="mb-4">
+          <label className="block text-sm font-medium">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+            className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
+            placeholder="Enter your password"
           />
         </div>
         <div className="mb-4">
           <label className="block text-sm font-medium">Document</label>
           <input
             type="text"
+            name="document"
+            value={formData.document}
+            onChange={handleChange}
             className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
             placeholder="Paste Your Link Here"
           />
         </div>
-        <div className="flex items-center mb-4">
+        <div className="mb-4">
+          <label className="block text-sm font-medium">GitHub</label>
           <input
-            type="checkbox"
-            className="h-4 w-4 text-red-500 focus:ring-red-400 border-gray-300 rounded"
+            type="text"
+            name="github"
+            value={formData.github}
+            onChange={handleChange}
+            className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
+            placeholder="https://github.com/sayyidz"
           />
-          <label className="ml-2 text-sm">I'm Ready To Start My Journey</label>
         </div>
         <button
           type="submit"
-          className="w-full bg-[#BA2025] text-white py-2 px-4 rounded-lg hover:bg-red-500 "
+          className="w-full bg-[#BA2025] text-white py-2 px-4 rounded-lg hover:bg-red-500"
         >
           SUBMIT
         </button>
