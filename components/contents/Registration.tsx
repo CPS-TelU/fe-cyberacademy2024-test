e6919a2c5db8d45d872e8d0f7089e111743bf44b
Hanntuu <raihanrrdarmawan@gmail.com>
2024-10-18 15:34:36 +0700

    refactor : new registration

--- components/contents/Registration.tsx
+++ components/contents/Registration.tsx
@@ -19,7 +19,7 @@ const RegistrationPage = () => {
     document: "",
     github: "",
   });
-
+  const [isReady, setIsReady] = useState(false); // State untuk checkbox
   const [showFacultyDropdown, setShowFacultyDropdown] = useState(false);
   const [showYearDropdown, setShowYearDropdown] = useState(false);
   const [showGenderDropdown, setShowGenderDropdown] = useState(false);
@@ -34,13 +34,19 @@ const RegistrationPage = () => {
 
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
-      if (facultyRef.current && !facultyRef.current.contains(event.target as Node)) {
+      if (
+        facultyRef.current &&
+        !facultyRef.current.contains(event.target as Node)
+      ) {
         setShowFacultyDropdown(false);
       }
       if (yearRef.current && !yearRef.current.contains(event.target as Node)) {
         setShowYearDropdown(false);
       }
-      if (genderRef.current && !genderRef.current.contains(event.target as Node)) {
+      if (
+        genderRef.current &&
+        !genderRef.current.contains(event.target as Node)
+      ) {
         setShowGenderDropdown(false);
       }
     };
@@ -80,7 +86,9 @@ const RegistrationPage = () => {
   };
 
   return (
-    <div className={`min-h-screen flex flex-col items-center justify-center p-4 ${poppins.className}`}>
+    <div
+      className={`min-h-screen flex flex-col items-center justify-center p-4 ${poppins.className}`}
+    >
       <div className="text-center mb-5">
         <h1 className="text-3xl md:text-4xl font-bold bg-gradient-to-r from-[#BA2025] to-[#133042] text-transparent bg-clip-text px-4 md:px-8">
           REGISTRATION FORM
@@ -89,7 +97,10 @@ const RegistrationPage = () => {
           The first step to start your journey
         </p>
       </div>
-      <form onSubmit={handleSubmit} className="bg-white px-6 md:px-16 py-8 rounded-2xl shadow-2xl w-full max-w-lg md:max-w-4xl mb-8 border border-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white px-6 md:px-16 py-8 rounded-2xl shadow-2xl w-full max-w-lg md:max-w-4xl mb-8 border border-gray-100"
+      >
         <div className="mb-4">
           <label className="block text-sm font-medium">Name</label>
           <input
@@ -201,7 +212,9 @@ const RegistrationPage = () => {
             )}
           </div>
           <div ref={yearRef} className="relative mb-4">
-            <label className="block text-sm font-medium">Year of Enrollment</label>
+            <label className="block text-sm font-medium">
+              Year of Enrollment
+            </label>
             <div
               className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg cursor-pointer"
               onClick={() => setShowYearDropdown(!showYearDropdown)}
@@ -237,44 +250,28 @@ const RegistrationPage = () => {
             placeholder="Telecommunication Engineering"
           />
         </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium">Password</label>
+
+        <div className="flex items-center mb-4 mt-4">
           <input
-            type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-            placeholder="Enter your password"
+            type="checkbox"
+            checked={isReady}
+            onChange={() => setIsReady(!isReady)} // Toggle checkbox state
+            className="h-4 w-4 text-red-500 focus:ring-red-400 border-gray-300 rounded"
           />
+          <label className="ml-2 text-sm">
+            I'm Ready To Start My Journey
+          </label>
         </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium">Document</label>
-          <input
-            type="text"
-            name="document"
-            value={formData.document}
-            onChange={handleChange}
-            className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-            placeholder="Paste Your Link Here"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="block text-sm font-medium">GitHub</label>
-          <input
-            type="text"
-            name="github"
-            value={formData.github}
-            onChange={handleChange}
-            className="w-full px-4 py-2 mt-2 border border-gray-300 rounded-lg focus:outline-none focus:border-red-500"
-            placeholder="https://github.com/sayyidz"
-          />
-        </div>
+
+        {/* Submit button */}
         <button
           type="submit"
-          className="w-full bg-[#BA2025] text-white py-2 px-4 rounded-lg hover:bg-red-500"
+          className={`w-full bg-[#BA2025] text-white font-bold py-2 px-4 rounded-lg ${
+            isReady ? "hover:bg-red-500" : "opacity-50 cursor-not-allowed"
+          }`}
+          disabled={!isReady} // Disable button if not checked
         >
-          SUBMIT
+          Submit
         </button>
       </form>
     </div>
